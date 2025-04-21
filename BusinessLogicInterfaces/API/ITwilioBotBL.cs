c5dfba56a735e7d797bccba427587b3f04c20258
Ismael Leon <ismaleonsaenz@gmail.com>
2025-04-20 22:53:55 -0600

    Twilio Implementation
    Code and classes to implement our WhatsApp Sandbox (Twilio)

---
+++ BusinessLogicInterfaces/API/ITwilioBotBL.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BusinessLogicInterfaces.API
+{  
+    /// Interface manage the twilio conecction API  
+     
+    public interface ITwilioBotBL
+    {
+        #region Get
+         
+        /// Method handle of get message that I sent from Whatsapp.
+         
+        /// <param name="Body"> Here came the question from Whatsapp</param> 
+        /// <param name="From"> Here came the number of Whatsapp from  whom sent message</param> 
+        /// <returns>A string containing the OpenIA response.</returns>    
+        IActionResult Get([FromForm] string Body, [FromForm] string From);
+        #endregion
+    }
+}
